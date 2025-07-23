f889b727d3b967afbd787f233ada653e1dec8a6d
Gilvam Gomes <ggomes.fhb@gmail.com>
2025-07-23 19:03:19 -0300

    Lógica de programação: mergulhe em programação com JavaScript
    Parte 1: Iniciando no JS.
    
    Lógica de programação básica.

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+alert('Olá, mundo!')
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+
+if (numeroSecreto == chute){
+    console.log('Acertou o número secreto ');
+}
+
